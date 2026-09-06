a3e4ae3bceea7a72d675085869d0637e2954f7dc
Memecoin Hunter <nyoussouf99@gmail.com>
2026-09-06 22:29:59 +0200

    Fix: Update App.tsx to match refactored component signatures

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,14 @@ import { Alerts } from "./pages/Alerts";
 import { useAppStore } from "./store/appStore";
 
 function App() {
-  const { activePage, modal } = useAppStore();
-
-  const handleSync = () => {
-    console.log("Data synced!");
-  };
+  const { activePage, modalState } = useAppStore();
 
   return (
     <div className="flex h-screen bg-dark-950 text-white overflow-hidden">
       <Sidebar />
 
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Header onSync={handleSync} />
+        <Header />
 
         <div className="flex-1 overflow-hidden">
           {activePage === "market" && <Market />}
@@ -29,8 +25,8 @@ function App() {
         </div>
       </div>
 
-      {modal.type === "coin" && <CoinModal coin={modal.data as any} isOpen={modal.isOpen} />}
-      {modal.type === "signal" && <SignalModal signal={modal.data as any} isOpen={modal.isOpen} />}
+      <CoinModal />
+      <SignalModal />
     </div>
   );
 }
